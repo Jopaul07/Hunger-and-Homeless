a7430f305555500a52bd10a578993fb3b9ee0697
Jopaul <pauljoseph95@gmail.com>
2017-09-18 18:06:31 +0530

    local commit

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,19 @@ router.get('/', function(req, res, next) {
 router.get('/test', function(req, res, next) {
   res.render('test', { title: 'Express' });
 });
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+router.get('/campaigns', function(req, res, next) {
+  res.render('pages/campaigns', { title: 'Express' });
+});
+router.get('/items', function(req, res, next) {
+  res.render('pages/items', { title: 'Express' });
+});
+router.get('/cat', function(req, res, next) {
+  res.render('pages/cat', { title: 'Express' });
+});
+router.get('/htest', function(req, res, next) {
+  res.render('htest', { title: 'Express' });
+});
 module.exports = router;
